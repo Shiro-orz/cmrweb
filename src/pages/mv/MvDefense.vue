3a328cda9c2918ff481b86c69f4c6aaa75bf7822
shiro-orz <shirotyan@qq.com>
2022-12-14 20:08:47 +0800

    update Multiview pages

--- src/pages/mv/MvDefense.vue
+++ src/pages/mv/MvDefense.vue
@@ -2,9 +2,36 @@
     <div :style="{ width: '100%', height: '100%' }">
       <div>
         <a-divider orientation="left">Attack MVCNN</a-divider>
+        <a-row>
+          <div :style="{width: '10%'}"></div>
         <div id="attackEchart" :style="{width: '800px', height: '400px'}"></div>
+        </a-row>
         <a-divider orientation="left">Defense Method: MAAE Vs Adversarial Training</a-divider>
+        <a-row>
+        <div :style="{width: '5%'}"></div>
+        <div>Choose adversarial data for MAAE or Adversarial Training:</div>
+        </a-row>
+        <a-radio-group
+          v-model:value="advtype"
+          name="radioGroup"
+          style="width: 100%"
+        >
+          <a-row>
+            <a-col :span="5">
+                <a-radio value="FGSM">FGSM</a-radio>
+            </a-col>
+            <a-col :span="5">
+                <a-radio value="BIM">BIM</a-radio>
+            </a-col>
+            <a-col :span="5">
+                <a-radio value="MIM">MIM</a-radio> 
+            </a-col>
+          </a-row>
+        </a-radio-group>
+        <a-row>
+          <div :style="{width: '10%'}"></div>
         <div id="defEchart" :style="{width: '800px', height: '400px'}"></div>
+        </a-row>
       </div>
      
     </div>
@@ -13,7 +40,7 @@
   <script>
   /* eslint-disable */
   
-  import { toRefs, defineComponent, reactive, ref, toRaw, onMounted } from "vue";
+  import { watch, toRefs, defineComponent, reactive, ref, toRaw, onMounted } from "vue";
   import * as echarts from 'echarts'
 
   export default defineComponent({
@@ -22,7 +49,20 @@
     },
 
     setup() {
-      
+      var advtype = ref('FGSM')
+
+      const MAAEdatas = {
+        'FGSM': [0.891, 0.893, 0.889],
+        'BIM': [0.896, 0.895, 0.8],
+        'MIM': [0.884, 0.884, 0.885]
+      }
+
+      const ATdatas = {
+        'FGSM': [0.943, 0.843, 0.843],
+        'BIM': [0.829, 0.928, 0.829],
+        'MIM': [0.823, 0.833, 0.934]
+      }
+
       const atOption = {
           title:{
             text: 'Acc of MVCNN',
@@ -43,7 +83,7 @@
         ]
       }
 
-      const defOption = {
+      var defOption = reactive({
             title:{
                 top: 0,
                 text: 'Acc of Two Defense Methods',
@@ -56,9 +96,9 @@
             dataset: {
               source: [
                 ['DefenseMethod', 'MAAE', 'AdvTraining'],
-                ['FGSM', 0.891, 0.858],
-                ['BIM', 0.895, 0.734],
-                ['MIM', 0.885, 0.652],
+                ['FGSM', MAAEdatas[advtype.value][0], ATdatas[advtype.value][0]],
+                ['BIM', MAAEdatas[advtype.value][1], ATdatas[advtype.value][1]],
+                ['MIM', MAAEdatas[advtype.value][2], ATdatas[advtype.value][2]],
               ]
             },
             xAxis: { type: 'category' },
@@ -66,19 +106,46 @@
             // Declare several bar series, each will be mapped
             // to a column of dataset.source by default.
             series: [{ type: 'bar' }, { type: 'bar' }]
-          };
+          });
 
       onMounted(() => {
+
+       
             var atChDom = document.getElementById('attackEchart')
             var atChart = echarts.init(atChDom)
             atOption && atChart.setOption(atOption)    
             var defChDom = document.getElementById('defEchart')
             var defChart = echarts.init(defChDom)
+            defChart.clear()
             defOption && defChart.setOption(defOption) 
             //     window.onresize = function () {
         //     myChart.resize()
         // }
         });
+
+        watch(
+            advtype,
+            () => {
+                var chartDom = document.getElementById('defEchart')
+                //const chartDom = ref("myEcharts")
+                var defChart = echarts.init(chartDom)// console.log('watch ...')
+                defChart.clear()
+                defOption.dataset.source[1][2] = ATdatas[advtype.value][0]
+                defOption.dataset.source[2][2] = ATdatas[advtype.value][1]
+                defOption.dataset.source[3][2] = ATdatas[advtype.value][2]
+                defOption.dataset.source[1][1] = MAAEdatas[advtype.value][0]
+                defOption.dataset.source[2][1] = MAAEdatas[advtype.value][1]
+                defOption.dataset.source[3][1] = MAAEdatas[advtype.value][2]
+                if (defChart) {
+                    defChart.setOption(defOption)
+                }
+            },
+            {deep: true}
+        )
+      
+      return {
+        advtype
+      }
 
     },
 
